47abb7b07bccc6f60a269701ab15c15e206493cb
Lorimer Jenkins <lorimerjenkins1@gmail.com>
2024-10-17 17:16:24 +0100

    feat: body + misc

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,14 @@
 "use client";
 import styles from "./page.module.css";
 import Header from "../components/Header/Header";
-// import HomeBody from "./Home/HomeBody";
 
 const Home = () => {
   return (
     <div className={styles.page}>
       <Header />
-      {/* <HomeBody /> */}
+      <div className={styles.body}>
+        lol
+      </div>
     </div>
   );
 };

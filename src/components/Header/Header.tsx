d9c2e5d24274945b7071fbe1760f9a7ec40f290a
Lorimer Jenkins <lorimerjenkins1@gmail.com>
2024-10-18 11:32:29 +0100

    feat: add input

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,7 +4,7 @@ import styles from "./Header.module.css";
 import Link from "next/link";
 import Image from "next/image";
 import { usePathname } from "next/navigation";
-import Connect from "../Dashboard/Connect/Connect";
+import Connect from "../Connect/Connect";
 
 const titleClick = () => {
   window.location.reload();
---
+++ src/components/Input/Input.tsx
@@ -0,0 +1,52 @@
+import React, { HTMLProps, useEffect, useState } from "react";
+import { Mail01 } from "@untitled-ui/icons-react";
+import styles from "./Input.module.css";
+
+interface Props {
+  status?: "error";
+  onEnter?: () => unknown;
+  labelText: string;
+}
+
+export default function Input({
+  value,
+  onChange,
+  status,
+  onEnter,
+  labelText,
+  ...props
+}: HTMLProps<HTMLInputElement> & Props) {
+  const [val, setVal] = useState<string | undefined>();
+
+  useEffect(() => {
+    if (val === value) return;
+    setVal(value?.toString());
+  }, [value]);
+
+  const hasContent = typeof val !== "undefined" && val !== "";
+
+  return (
+    <div className={`${styles.wrapper} ${status === "error" ? styles.error : ""}`}>
+      <div className={styles.content}>
+        <p className={`${styles.label} ${hasContent ? styles.hasContent : ""}`}>
+          {labelText}
+        </p>
+        <Mail01 className={styles.icon} />
+      </div>
+      <input
+        type="email"
+        className={styles.input}
+        value={val}
+        onChange={(e) => {
+          setVal(e.target.value);
+          if (onChange) onChange(e);
+        }}
+        onKeyDown={(e) => {
+          if (e.key !== "Enter" || !onEnter) return;
+          onEnter();
+        }}
+        {...props}
+      />
+    </div>
+  );
+}

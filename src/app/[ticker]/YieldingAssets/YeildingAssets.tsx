2b087ef5c0c7c0c03b07aa7e9d3b1fbf7959182d
Lorimer Jenkins <lorimerjenkins1@gmail.com>
2024-10-25 12:39:07 +0100

    feat: no assets

--- src/app/[ticker]/YieldingAssets/YeildingAssets.tsx
+++ src/app/[ticker]/YieldingAssets/YeildingAssets.tsx
@@ -70,6 +70,8 @@ const assets: Asset[] = [
 ];
 
 const YieldingAssets: React.FC = () => {
+  const maxYield = "5.1";
+
   const [showAll, setShowAll] = useState(false);
 
   const displayedAssets = showAll ? assets : assets.slice(0, 4);
@@ -82,71 +84,88 @@ const YieldingAssets: React.FC = () => {
     <div className={`${styles.container} ${showAll ? styles.expanded : ""}`}>
       <div className={styles.header}>
         <h2 className={styles.title}>Yielding assets</h2>
-        {showAll ? (
-          <button onClick={toggleView} className={styles.closeButton}>
-            <Image src="/icons/close.svg" alt="Close" width={9} height={9} />
-          </button>
-        ) : (
-          <button onClick={toggleView} className={styles.viewAll}>
-            View all
-          </button>
-        )}
-      </div>
-
-      <div className={styles.assetsList}>
-        {displayedAssets.map((asset, index) => (
-          <div
-            key={`${asset.name}-${index}`}
-            className={styles.assetRowWrapper}
-          >
-            <div className={styles.assetRow}>
-              <div className={styles.assetInfo}>
-                <div className={styles.iconWrapper}>
-                  <Image
-                    src={asset.icon}
-                    alt={asset.name}
-                    width={40}
-                    height={40}
-                  />
-                </div>
-                <div className={styles.nameAmount}>
-                  <p className={styles.name}>{asset.name}</p>
-                  <p className={styles.amount}>
-                    {asset.amount} {asset.symbol}
-                  </p>
-                </div>
-              </div>
-
-              <div className={styles.aprInfo}>
-                <p className={styles.apr}>APR {asset.apr}%</p>
-                <div className={styles.changeInfo}>
-                  <Image
-                    src={
-                      asset.isPositive
-                        ? "/icons/APRUp.svg"
-                        : "/icons/APRDown.svg"
-                    }
-                    alt="APR change indicator"
-                    width={16}
-                    height={16}
-                  />
-                  <p className={styles.change}>{asset.change}%</p>
-                </div>
-              </div>
-            </div>
-            <button className={styles.withdrawButton}>
-              <Image
-                src="/icons/withdraw.svg"
-                alt="Withdraw"
-                width={14}
-                height={14}
-                className={styles.withdrawIcon}
-              />
-              <span>Withdraw</span>
+        {assets.length > 4 &&
+          (showAll ? (
+            <button onClick={toggleView} className={styles.closeButton}>
+              <Image src="/icons/close.svg" alt="Close" width={9} height={9} />
             </button>
-          </div>
-        ))}
+          ) : (
+            <button onClick={toggleView} className={styles.viewAll}>
+              View all
+            </button>
+          ))}
       </div>
+
+      {assets.length === 0 ? (
+        <div className={styles.emptyState}>
+          <Image
+            src="/icons/noAssets.png"
+            alt="No assets"
+            width={120}
+            height={120}
+            className={styles.emptyStateIcon}
+          />
+          <h3 className={styles.emptyStateTitle}>No assets supplied yet</h3>
+          <p className={styles.emptyStateText}>
+            Providing collateral can earn you up to {maxYield}% APY
+          </p>
+        </div>
+      ) : (
+        <div className={styles.assetsList}>
+          {displayedAssets.map((asset, index) => (
+            <div
+              key={`${asset.name}-${index}`}
+              className={styles.assetRowWrapper}
+            >
+              <div className={styles.assetRow}>
+                <div className={styles.assetInfo}>
+                  <div className={styles.iconWrapper}>
+                    <Image
+                      src={asset.icon}
+                      alt={asset.name}
+                      width={40}
+                      height={40}
+                    />
+                  </div>
+                  <div className={styles.nameAmount}>
+                    <p className={styles.name}>{asset.name}</p>
+                    <p className={styles.amount}>
+                      {asset.amount} {asset.symbol}
+                    </p>
+                  </div>
+                </div>
+
+                <div className={styles.aprInfo}>
+                  <p className={styles.apr}>APR {asset.apr}%</p>
+                  <div className={styles.changeInfo}>
+                    <Image
+                      src={
+                        asset.isPositive
+                          ? "/icons/APRUp.svg"
+                          : "/icons/APRDown.svg"
+                      }
+                      alt="APR change indicator"
+                      width={16}
+                      height={16}
+                    />
+                    <p className={styles.change}>{asset.change}%</p>
+                  </div>
+                </div>
+              </div>
+              <button className={styles.withdrawButton}>
+                <Image
+                  src="/icons/withdraw.svg"
+                  alt="Withdraw"
+                  width={14}
+                  height={14}
+                  className={styles.withdrawIcon}
+                />
+                <span>Withdraw</span>
+              </button>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 };

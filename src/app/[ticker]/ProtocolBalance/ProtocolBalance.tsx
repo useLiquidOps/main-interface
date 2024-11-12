e0d459e8b2cd10a6ba6c3b0be7931144355a90a5
Lorimer Jenkins <lorimerjenkins1@gmail.com>
2024-11-12 16:04:23 +0000

    fezt: get rid of graph tooltip

--- src/app/[ticker]/ProtocolBalance/ProtocolBalance.tsx
+++ src/app/[ticker]/ProtocolBalance/ProtocolBalance.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from 'react';
 import {
   LineChart,
   Line,
@@ -6,7 +6,6 @@ import {
   Tooltip,
   XAxis,
   YAxis,
-  TooltipProps,
 } from "recharts";
 import styles from "./ProtocolBalance.module.css";
 import Image from "next/image";
@@ -17,6 +16,10 @@ const ProtocolBalance: React.FC<{
   ticker: string;
 }> = ({ ticker }) => {
   const router = useRouter();
+  const [hoverData, setHoverData] = useState<{
+    date: string;
+    value: number;
+  } | null>(null);
 
   const tokenData = headerTokensData.find(
     (token) => token.ticker.toLowerCase() === ticker.toLowerCase(),
@@ -65,20 +68,26 @@ const ProtocolBalance: React.FC<{
       </div>
       <div className={styles.aprSection}>
         <div className={styles.aprContainer}>
-          <p className={styles.aprLabel}>APR</p>
+          <p className={styles.aprLabel}>
+            {hoverData ? hoverData.date : 'APR'}
+          </p>
           <div className={styles.aprValue}>
-            <Image
-              src={
-                tokenData.percentChange.outcome
-                  ? "/icons/APRUp.svg"
-                  : "/icons/APRDown.svg"
-              }
-              alt={tokenData.percentChange.outcome ? "Up Arrow" : "Down Arrow"}
-              width={0}
-              height={16}
-              style={{ width: "auto", height: "16px" }}
-            />
-            <p className={styles.aprText}>{tokenData.APR}%</p>
+            {!hoverData && (
+              <Image
+                src={
+                  tokenData.percentChange.outcome
+                    ? "/icons/APRUp.svg"
+                    : "/icons/APRDown.svg"
+                }
+                alt={tokenData.percentChange.outcome ? "Up Arrow" : "Down Arrow"}
+                width={0}
+                height={16}
+                style={{ width: "auto", height: "16px" }}
+              />
+            )}
+            <p className={styles.aprText}>
+              {hoverData ? `${hoverData.value}%` : `${tokenData.APR}%`}
+            </p>
           </div>
         </div>
         <div className={styles.graph}>
@@ -86,6 +95,15 @@ const ProtocolBalance: React.FC<{
             <LineChart
               data={graphDummyData}
               margin={{ top: 0, right: 0, left: 0, bottom: 0 }}
+              onMouseMove={(data) => {
+                if (data.activePayload) {
+                  setHoverData({
+                    date: data.activeLabel as string,
+                    value: data.activePayload[0].value as number,
+                  });
+                }
+              }}
+              onMouseLeave={() => setHoverData(null)}
             >
               <defs>
                 <linearGradient
@@ -115,33 +133,13 @@ const ProtocolBalance: React.FC<{
                 strokeWidth={3}
                 dot={false}
               />
-              <Tooltip
-                content={<CustomTooltip />}
-                cursor={false}
-                position={{ y: -40 }}
-              />
+              <Tooltip content={<></>} cursor={false} />
             </LineChart>
           </ResponsiveContainer>
         </div>
       </div>
     </div>
   );
-};
-
-const CustomTooltip: React.FC<TooltipProps<number, string>> = ({
-  active,
-  payload,
-  label,
-}) => {
-  if (active && payload && payload.length) {
-    return (
-      <div className={styles.customTooltip}>
-        <p className={styles.tooltipDate}>{label}</p>
-        <p className={styles.tooltipValue}>{`APR : ${payload[0].value}%`}</p>
-      </div>
-    );
-  }
-  return null;
 };
 
 export default ProtocolBalance;

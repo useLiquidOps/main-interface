0c0aa111082a01e7f1829d3faf9ebd69119367dd
Lorimer Jenkins <lorimerjenkins1@gmail.com>
2024-11-12 16:19:23 +0000

    chore: delete wallet kit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,10 +2,6 @@ import { DM_Sans } from "next/font/google";
 import "./globals.css";
 import { Metadata } from "next";
 import { ModalProvider } from "./[ticker]/PopUp/PopUp";
-// import { ArweaveWalletKit } from "@arweave-wallet-kit-beta/react";
-// import BrowserWalletStrategy from "@arweave-wallet-kit-beta/browser-wallet-strategy";
-// import ArConnectStrategy from "@arweave-wallet-kit-beta/arconnect-strategy";
-// import OthentStrategy from "@arweave-wallet-kit-beta/othent-strategy";
 
 export const metadata: Metadata = {
   title: "LiquidOps | Home",
@@ -28,34 +24,15 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      {/* <ArweaveWalletKit
-        config={{
-          strategies: [
-            new ArConnectStrategy(),
-            // new OthentStrategy(),
-            new BrowserWalletStrategy(),
-          ],
-          permissions: ["ACCESS_ADDRESS", "SIGN_TRANSACTION"],
-          ensurePermissions: true,
-          appInfo: {
-            name: "LiquidOps",
-            logo: "/favicon.svg",
-          },
-        }}
-        theme={{
-          displayTheme: "light",
-          accent: { r: 72, g: 68, b: 236 },
-          titleHighlight: { r: 72, g: 68, b: 236 },
-          radius: "default",
-        }}
-      > */}
+
+
       <ModalProvider>
         <body className={dmSans.className} style={{ margin: 0, padding: 0 }}>
           {children}
         </body>
       </ModalProvider>
 
-      {/* </ArweaveWalletKit> */}
+
     </html>
   );
 }

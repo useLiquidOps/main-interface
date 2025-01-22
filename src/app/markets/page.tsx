742a53e56f144fef6410f35a23add488b6c45431
Lorimer Jenkins <lorimerjenkins1@gmail.com>
2025-01-22 14:34:28 +0000

    fix: window undef error

--- src/app/markets/page.tsx
+++ src/app/markets/page.tsx
@@ -1,17 +1,22 @@
-import Markets from "./markets";
 import { Metadata } from "next";
+import dynamic from 'next/dynamic';
 
 export const metadata: Metadata = {
-  title: "LiquidOps | Markets",
-  icons: {
-    icon: "/favicon.svg",
-  },
-  description:
-    "LiquidOps is an over-collateralised lending and borrowing protocol built on Arweave's L2 AO.",
+ title: "LiquidOps | Markets",
+ icons: {
+   icon: "/favicon.svg",
+ },
+ description:
+   "LiquidOps is an over-collateralised lending and borrowing protocol built on Arweave's L2 AO.",
 };
 
+// Dynamically import with SSR disabled, fix window error
+const Markets = dynamic(() => import("./markets"), {
+ ssr: false
+});
+
 const Page = () => {
-  return <Markets />;
+ return <Markets />;
 };
 
 export default Page;

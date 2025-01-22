6c96647fc89f88143dc65a2c1d3d74b587961b63
Marton Lederer <marton@lederer.hu>
2025-01-22 15:51:16 +0100

    feat: remove link command

--- src/app/markets/page.tsx
+++ src/app/markets/page.tsx
@@ -1,22 +1,22 @@
 import { Metadata } from "next";
-import dynamic from 'next/dynamic';
+import dynamic from "next/dynamic";
 
 export const metadata: Metadata = {
- title: "LiquidOps | Markets",
- icons: {
-   icon: "/favicon.svg",
- },
- description:
-   "LiquidOps is an over-collateralised lending and borrowing protocol built on Arweave's L2 AO.",
+  title: "LiquidOps | Markets",
+  icons: {
+    icon: "/favicon.svg",
+  },
+  description:
+    "LiquidOps is an over-collateralised lending and borrowing protocol built on Arweave's L2 AO.",
 };
 
 // Dynamically import with SSR disabled, fix window error
 const Markets = dynamic(() => import("./markets"), {
- ssr: false
+  ssr: false,
 });
 
 const Page = () => {
- return <Markets />;
+  return <Markets />;
 };
 
 export default Page;

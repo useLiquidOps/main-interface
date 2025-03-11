361627b7c34c65e68576efb5f56153e21825f740
Lorimer Jenkins <lorimerjenkins1@gmail.com>
2025-03-11 11:05:31 +0000

    feat: hash routing

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  assetPrefix: "./",
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+};
 
 export default nextConfig;

7871bfd2293b082317bad8a950c7acbaa97bb5a5
Lorimer Jenkins <lorimerjenkins1@gmail.com>
2025-03-19 11:32:49 +0000

    fix: dynamically import components

--- src/app/[ticker]/borrow/page.tsx
+++ src/app/[ticker]/borrow/page.tsx
@@ -1,5 +1,5 @@
-import Borrow from "./Borrow";
 import { Metadata } from "next";
+import dynamic from "next/dynamic";
 
 export const metadata: Metadata = {
   title: "LiquidOps | Borrow",
@@ -9,6 +9,11 @@ export const metadata: Metadata = {
   description:
     "LiquidOps is an over-collateralised lending and borrowing protocol built on Arweave's L2 AO.",
 };
+
+// Dynamically import with SSR disabled, fix window error
+const Borrow = dynamic(() => import("./Borrow"), {
+  ssr: false,
+});
 
 const Page = ({ params }: { params: { ticker: string; tab: string } }) => {
   const ticker = params.ticker as string;

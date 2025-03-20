bd2fd1b2f9a5271d0776067d78c7759db9bbdf1d
Lorimer Jenkins <lorimerjenkins1@gmail.com>
2025-03-20 12:23:20 +0000

    feat: send feedback button

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -7,43 +7,54 @@ const Footer = () => {
 
   return (
     <div className={styles.footer}>
-      <div className={styles.version}>
-        <p>v0.1.0</p>
-        <p>·</p>
-        <p>{gitHash}</p>
+      <div className={styles.left}>
+        <a
+          href="https://forms.gle/nzErNbwpq1w4gPEt8"
+          className={styles.feedback}
+          target="_blank"
+        >
+          Send feedback
+        </a>
+
+        <div className={styles.version}>
+          <p>v0.1.0</p>
+          <p>{gitHash}</p>
+        </div>
       </div>
-      <div className={styles.socials}>
-        <a
-          href="https://x.com/Liquid_Ops"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image src="/socials/x.svg" alt="X" height={15} width={15} />
-        </a>
-        <a
-          href="https://discord.gg/Jad4v8ykgY"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            src="/socials/discord.svg"
-            alt="Discord"
-            height={15}
-            width={15}
-          />
-        </a>
-        <a
-          href="https://github.com/useLiquidOps"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            src="/socials/github.svg"
-            alt="GitHub"
-            height={15}
-            width={15}
-          />
-        </a>
+      <div className={styles.right}>
+        <div className={styles.socials}>
+          <a
+            href="https://x.com/Liquid_Ops"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image src="/socials/x.svg" alt="X" height={15} width={15} />
+          </a>
+          <a
+            href="https://discord.gg/Jad4v8ykgY"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              src="/socials/discord.svg"
+              alt="Discord"
+              height={15}
+              width={15}
+            />
+          </a>
+          <a
+            href="https://github.com/useLiquidOps"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              src="/socials/github.svg"
+              alt="GitHub"
+              height={15}
+              width={15}
+            />
+          </a>
+        </div>
       </div>
     </div>
   );

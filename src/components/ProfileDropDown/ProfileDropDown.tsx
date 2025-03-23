6aa9171dffeb4a041a0a1b90480a2a40fb3793cb
Lorimer Jenkins <lorimerjenkins1@gmail.com>
2025-03-23 13:43:46 +0000

    feat: skeleton loading on profile pic

--- src/components/ProfileDropDown/ProfileDropDown.tsx
+++ src/components/ProfileDropDown/ProfileDropDown.tsx
@@ -6,6 +6,7 @@ import { dropdownVariants } from "@/components/DropDown/FramerMotion";
 import styles from "./ProfileDropDown.module.css";
 import ActivityList from "../ActivityList/ActivityList";
 import { useTransactions } from "@/hooks/data/useTransactions";
+import { SkeletonLoading } from "@/components/SkeletonLoading/SkeletonLoading";
 
 interface ProfileDropdownProps {
   isOpen: boolean;
@@ -48,21 +49,27 @@ const ProfileDropDown: React.FC<ProfileDropdownProps> = ({
           <p className={styles.title}>Profile</p>
           <div className={styles.profileHeader}>
             <div className={styles.profileDetails}>
-              <img
-                src={
-                  !isProfileLoading && profile?.thumbnail
-                    ? `https://arweave.net/${profile.thumbnail}`
-                    : "/icons/user.svg"
-                }
-                alt="Profile image"
-                width={42}
-                height={42}
-                className={styles.profileImage}
-                onError={(e) => {
-                  const target = e.target as HTMLImageElement;
-                  target.src = "/icons/user.svg";
-                }}
-              />
+              <div className={styles.profileImageContainer}>
+                {isProfileLoading ? (
+                  <SkeletonLoading className="h-full w-full rounded-full" />
+                ) : (
+                  <img
+                    src={
+                      profile?.thumbnail
+                        ? `https://arweave.net/${profile.thumbnail}`
+                        : "/icons/user.svg"
+                    }
+                    alt="Profile image"
+                    width={42}
+                    height={42}
+                    className={styles.profileImage}
+                    onError={(e) => {
+                      const target = e.target as HTMLImageElement;
+                      target.src = "/icons/user.svg";
+                    }}
+                  />
+                )}
+              </div>
               <div className={styles.profileName}>
                 <p className={styles.userName}>
                   {!isProfileLoading && profile?.username

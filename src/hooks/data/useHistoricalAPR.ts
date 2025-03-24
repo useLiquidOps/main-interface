df2d767d627a561f283cd00a24b9ecfc80cbe15a
Lorimer Jenkins <lorimerjenkins1@gmail.com>
2025-03-24 16:01:57 +0000

    chore: update use historical apr hook

--- src/hooks/data/useHistoricalAPR.ts
+++ src/hooks/data/useHistoricalAPR.ts
@@ -1,42 +1,39 @@
 import { useQuery } from "@tanstack/react-query";
 import { LiquidOpsClient } from "@/utils/LiquidOps";
-
-interface HistoricalAPRData {
-  date: string;
-  value: number;
-}
+import { GetHistoricalAPRRes } from "liquidops";
 
 export function useHistoricalAPR(token: string) {
   return useQuery({
     queryKey: ["historical-apr", token],
-    queryFn: async (): Promise<HistoricalAPRData[]> => {
-      try {
-        const data = await LiquidOpsClient.getHistoricalAPR({
-          token,
-        });
+    queryFn: async (): Promise<formatedHistoricalAPRRes[]> => {
+      const data = await LiquidOpsClient.getHistoricalAPR({
+        token,
+      });
 
-        return data
-          .map((item) => ({
-            date: new Date(item.timestamp).toLocaleDateString(),
-            value: item.apr,
-          }))
-          .sort(
-            (a, b) => new Date(a.date).getTime() - new Date(b.date).getTime(),
-          );
-      } catch (error) {
-        // If historical data fails, fetch current APR as fallback
-        const currentAPR = await LiquidOpsClient.getAPR({ token });
-
-        // Return array with single entry for current APR
-        return [
-          {
-            date: new Date().toLocaleDateString(),
-            value: Number(currentAPR.toFixed(2)),
-          },
-        ];
-      }
+      return formatHistoricalAPR(data);
     },
     staleTime: 30 * 1000,
     gcTime: 5 * 60 * 1000,
   });
+}
+
+interface formatedHistoricalAPRRes {
+  date: string;
+  value: number;
+}
+
+export function formatHistoricalAPR(data: GetHistoricalAPRRes) {
+  if (data.length > 2) {
+    return data
+      .map((item) => ({
+        date: new Date(item.timestamp).toLocaleDateString(),
+        value: item.apr,
+      }))
+      .sort((a, b) => new Date(a.date).getTime() - new Date(b.date).getTime());
+  } else {
+    return data.map((item) => ({
+      date: new Date(item.timestamp).toLocaleDateString(),
+      value: item.apr,
+    }));
+  }
 }

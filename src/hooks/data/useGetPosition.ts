9689d46917a5d13bbe4ee6464af873cfc0abeb6a
Lorimer Jenkins <lorimerjenkins1@gmail.com>
2025-03-24 15:58:24 +0000

    fix: useGetPosition

--- src/hooks/data/useGetPosition.ts
+++ src/hooks/data/useGetPosition.ts
@@ -6,14 +6,13 @@ import { Token, Quantity } from "ao-tokens";
 export function useGetPosition(tokenAddress: string) {
   const { data: walletAddress } = useWalletAddress();
 
-  // return empty position, if the user has not connected their wallet address
-  if (!walletAddress) {
-    return new Quantity(0n, 12n);
-  }
-
   return useQuery({
     queryKey: ["position", tokenAddress, walletAddress],
     queryFn: async (): Promise<Quantity> => {
+      if (!walletAddress) {
+        return new Quantity(0n, 12n);
+      }
+      
       const tokenInstance = await Token(tokenAddress);
       const data = await LiquidOpsClient.getPosition({
         token: tokenAddress,
@@ -21,7 +20,7 @@ export function useGetPosition(tokenAddress: string) {
       });
       return new Quantity(data.borrowBalance, tokenInstance.info.Denomination);
     },
-    enabled: !!tokenAddress && !!walletAddress,
+    enabled: !!tokenAddress,
     staleTime: 30 * 1000,
     gcTime: 5 * 60 * 1000,
   });

705788fa93942293f01f493d134e249824b62df7
Lorimer Jenkins <lorimerjenkins1@gmail.com>
2025-04-02 21:36:59 +0100

    chore: remove liquidops comments

--- src/utils/LiquidOps.ts
+++ src/utils/LiquidOps.ts
@@ -1,9 +1,8 @@
 "use client";
-// @ts-ignore, due to bun link not importing types
 import LiquidOps from "liquidops";
 import { createDataItemSigner } from "@permaweb/aoconnect";
 
-const arweaveWallet = window.arweaveWallet; // TODO: Fix this and add real wallet handling
+const arweaveWallet = window.arweaveWallet;
 const signer = createDataItemSigner(arweaveWallet);
 
 export const LiquidOpsClient = new LiquidOps(signer);

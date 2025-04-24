e19b1d3a7416f7b4429624158617836b28ca6ea3
Lorimer Jenkins <lorimerjenkins1@gmail.com>
2025-04-25 00:03:13 +0100

    fix: disable scroll when profile open

--- src/components/Connect/Connect.tsx
+++ src/components/Connect/Connect.tsx
@@ -28,6 +28,11 @@ const Connect: React.FC = () => {
 
   const { isOpen, setAccountTab } = useAccountTab();
 
+  useEffect(() => {
+    document.body.style.overflow = isOpen ? 'hidden' : '';
+    return () => { document.body.style.overflow = ''; };
+  }, [isOpen]); // disable scroll when profile is visible
+
   const { data: profile, isLoading: isProfileLoading } = useAOProfile();
 
   const { ref: dropdownRef } = useClickOutside<HTMLDivElement>(() =>

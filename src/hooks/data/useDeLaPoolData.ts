a2eba8461b4d417b8d8ed60e2ccad8370ca9da7a
Lorimer Jenkins <lorimerjenkins1@gmail.com>
2025-04-28 18:19:26 +0100

    -

--- src/hooks/data/useDeLaPoolData.ts
+++ src/hooks/data/useDeLaPoolData.ts
@@ -4,7 +4,7 @@ import { isDataCachedValid, cacheData } from "@/utils/cacheUtils";
 
 const defiLlamaIds: Record<string, string> = {
   qAR: "cd2164c7-66f3-455c-a690-e4fc778f8a72",
-  wUSDC: "",
+  wUSDC: "55e444cd-dae7-4ad6-8a3b-1ca4d1d44bae",
 };
 
 export function useDeLaPoolData(ticker: string, overrideCache?: boolean) {

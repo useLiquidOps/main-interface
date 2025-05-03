bdeece1d9f1db60fbb2010b7aa0250cefb20db4e
Lorimer Jenkins <lorimerjenkins1@gmail.com>
2025-05-03 22:49:33 +0100

    feat: add fund node function

--- scripts/permaweb-deploy.mjs
+++ scripts/permaweb-deploy.mjs
@@ -46,6 +46,15 @@ async function deploy() {
     });
     irys.uploader.useChunking = false;
 
+    // const arweaveTokenAmount = irys.utils.toAtomic(1)
+    // const fundNode = await irys.fund(arweaveTokenAmount)
+    // console.log(
+    //   "📜 LOG > funded:",
+    //   irys.utils.fromAtomic(arweaveTokenAmount),
+    //   irys.token,
+    //   fundNode
+    // );
+
     const balance = await irys.getBalance(DEPLOY_WALLET);
     console.log(
       "📜 LOG > node balance:",

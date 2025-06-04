c0dd68f94623f8f7a6935c445f926320e418032b
Lorimer Jenkins <lorimerjenkins1@gmail.com>
2025-06-04 23:10:25 +0100

    fix: useSupported tokens TS issue

--- src/hooks/data/useSupportedTokens.ts
+++ src/hooks/data/useSupportedTokens.ts
@@ -1,29 +1,17 @@
 import { useQuery } from "@tanstack/react-query";
 import { tokenData, TokenData } from "liquidops";
 
-export interface SupportedToken
-  extends Omit<
-    TokenData,
-    "oTicker" | "oAddress" | "controllerAddress" | "cleanTicker"
-  > {
-  denomination: bigint;
-  collateralEnabled: boolean;
-  baseDenomination: bigint;
-}
+export type SupportedToken = TokenData & {
+  icon: string;
+};
 
 export function useSupportedTokens() {
   return useQuery({
     queryKey: ["supported-tokens"],
     queryFn: (): SupportedToken[] => {
       return Object.entries(tokenData).map(([_, data]) => ({
+        ...data,
         icon: `./tokens/${data.cleanTicker}.svg`,
-        name: data.name,
-        ticker: data.cleanTicker,
-        denomination: data.denomination,
-        collateralEnabled: data.collateralEnabled,
-        baseDenomination: data.baseDenomination,
-        address: data.address,
-        oAddress: data.oAddress,
       }));
     },
     staleTime: 5 * 60 * 1000,

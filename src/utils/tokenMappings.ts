75ddb6ea3b9a1aa8c5826c573e6a6e22f7743fdb
Lorimer Jenkins <lorimerjenkins1@gmail.com>
2025-07-17 01:12:51 +0200

    -

--- src/utils/tokenMappings.ts
+++ src/utils/tokenMappings.ts
@@ -1,3 +1,5 @@
+// check marketStats.tsx for another data item
+
 export const defiLlamaIds: Record<string, string> = {
   qAR: "cd2164c7-66f3-455c-a690-e4fc778f8a72",
   wUSDC: "55e444cd-dae7-4ad6-8a3b-1ca4d1d44bae",

2a08b3012d9596e8ed2caad08f32a38b17a3d7d7
Marton Lederer <marton@lederer.hu>
2025-07-17 13:15:34 +0200

    fix: refresh site button

--- src/components/ClearCache/ClearCache.tsx
+++ src/components/ClearCache/ClearCache.tsx
@@ -1,33 +1,19 @@
 import React from "react";
 import styles from "./ClearCache.module.css";
+import { useQueryClient } from "@tanstack/react-query";
+import { del } from "idb-keyval";
 
 const ClearCache = () => {
-  const dataToNotClear = ["analytics-consent", "betaDisclaimer", "site_hints"];
-
-  const handleClearCache = () => {
-    // Save the items we want to keep
-    const savedItems: Record<string, string> = {};
-    dataToNotClear.forEach((key) => {
-      const value = localStorage.getItem(key);
-      if (value !== null) {
-        savedItems[key] = value;
-      }
-    });
-
-    // Clear everything in localStorage
-    localStorage.clear();
-
-    // Restore the items we want to keep
-    Object.entries(savedItems).forEach(([key, value]) => {
-      localStorage.setItem(key, value);
-    });
-
-    // // Refresh the page
-    window.location.reload();
-  };
+  const queryClient = useQueryClient();
 
   return (
-    <button className={styles.button} onClick={handleClearCache}>
+    <button
+      className={styles.button}
+      onClick={async () => {
+        await del("REACT_QUERY_OFFLINE_CACHE");
+        window.location.reload();
+      }}
+    >
       Refresh site
     </button>
   );

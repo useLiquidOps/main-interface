d1717b922b6d0ca86bbda3dfba4dc6a7c7e1dd9b
Lorimer Jenkins <lorimerjenkins1@gmail.com>
2025-07-17 03:47:34 +0200

    chore: temp disable the strategies page

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -95,12 +95,12 @@ const Header: React.FC<HeaderProps> = ({
             >
               <p>Markets</p>
             </Link>
-            <Link
+            {/* <Link
               href="/strategies"
               className={isLinkActive("/strategies") ? styles.activeLink : ""}
             >
               <p>Strategies</p>
-            </Link>
+            </Link> */}
             <MoreDropdown label="Bridge" items={bridgeItems} />
             <MoreDropdown label="More" items={moreMenuItems} />
           </nav>

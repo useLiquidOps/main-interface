8fa4ffac32f764a2b5914b2e60d32cf9f058505e
Lorimer Jenkins <lorimerjenkins1@gmail.com>
2025-08-27 12:44:05 +0200

    feat: new LiquidOps CU

--- src/utils/LiquidOps/LiquidOps.ts
+++ src/utils/LiquidOps/LiquidOps.ts
@@ -5,4 +5,6 @@ import { createDataItemSigner } from "@permaweb/aoconnect";
 const arweaveWallet = window.arweaveWallet;
 const signer = createDataItemSigner(arweaveWallet);
 
-export const LiquidOpsClient = new LiquidOps(signer);
+export const LiquidOpsClient = new LiquidOps(signer, {
+  CU_URL: "https://srv768410.hstgr.cloud/",
+});

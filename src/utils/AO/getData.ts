7369564ac3956d3ad9ecd968c041af5c6ce21ecb
Lorimer Jenkins <lorimerjenkins1@gmail.com>
2025-08-27 14:35:04 +0200

    -

--- src/utils/AO/getData.ts
+++ src/utils/AO/getData.ts
@@ -1,7 +1,6 @@
 import { dryrun } from "@permaweb/aoconnect";
 import { DryRunResult } from "node_modules/@permaweb/aoconnect/dist/lib/dryrun";
 
-
 interface MessageTags {
   Target: string;
   Action: string;
